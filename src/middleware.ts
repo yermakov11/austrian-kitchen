9e650cd1ebc8c641d14e45bc56bad241a76d263e
unknown <thumpel81@gmail.com>
2025-10-02 00:19:15 +0200

    changed token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,24 @@
-import { getToken } from "next-auth/jwt"
+import { getToken, GetTokenParams} from "next-auth/jwt"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
 
-  const token = await getToken({
-    req: request,
-    secret: process.env.AUTH_SECRET, 
-  })
+  let params: GetTokenParams = {
+      req: request,
+      secret: process.env.AUTH_SECRET ?? "secret"
+  }
+
+  if(process.env.NODE_ENV === "production"){
+      params = {
+        ...params,
+        cookieName: "__Secure-authjs.session-token"
+      }
+  }
+
+
+  const token = await getToken(params)
 
   const protectedRoutes = ["/ingredients"];
 
